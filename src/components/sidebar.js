1020ac558fb41f974ea38430b063894cbcd8a8e8
Ruslana Tarasova <audit2013@gmail.com>
2024-04-07 15:40:23 +0300

    hw-36

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -9,10 +9,10 @@ class Sidebar extends React.Component {
         return (
             <nav>
                 <ul>
-                    <li className="link"><a href="https://ru.legacy.reactjs.org/docs/create-a-new-react-app.html" target="_blank">React</a></li>
-                    <li><a href="https://ithillel.ua/ru"
+                    <li><a className="link" href="https://ru.legacy.reactjs.org/docs/create-a-new-react-app.html" target="_blank">React</a></li>
+                    <li><a className="link" href="https://ithillel.ua/ru"
                            target="_blank">Hillel IT school</a></li>
-                    <li><a href="https://create-react-app.dev/"
+                    <li><a className="link" href="https://create-react-app.dev/"
                            target="_blank">Create-react-app</a></li>
                 </ul>
             </nav>
